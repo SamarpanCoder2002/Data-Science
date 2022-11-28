c14350ee5d935ffb4f705e7615f4d2a87bb7616a
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2022-11-28 17:29:50 +0530

    Updated

--- aps_fail_project/sensor/entity/config_entity.py
+++ aps_fail_project/sensor/entity/config_entity.py
@@ -1,11 +1,39 @@
-class DataIngestionConfig:...
+import os
 
-class DataValidationConfig:...
+FILE_NAME = "sensor.csv"
 
-class DataTransformationConfig:...
 
-class ModelTrainerConfig:...
+class TrainingPipelineConfig:
+    def __init__(self):
+        self.artifact_dir = os.path.join(
+            os.getcwd(), 'artifact', f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
 
-class ModelEvaluationConfig:...
 
-class ModelPusherConfig:...
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.database = "aps"
+        self.collection_name = "sensor"
+        self.data_ingestion_dir = os.path.join(
+            training_pipeline_config.artifact_dir, "data_ingestion")
+        self.feature_store_dir = os.path.join(
+            self.data_ingestion_dir, 'feature_store', FILE_NAME)
+
+
+class DataValidationConfig:
+    ...
+
+
+class DataTransformationConfig:
+    ...
+
+
+class ModelTrainerConfig:
+    ...
+
+
+class ModelEvaluationConfig:
+    ...
+
+
+class ModelPusherConfig:
+    ...
